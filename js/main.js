ef56ec6124c953c0c96645c3766f6654c9c7b15a
tdk-123 <tdekoning87@gmail.com>
2026-09-18 07:37:00 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -304,7 +304,7 @@ if (fullMapEl) {
                     stops.push({
                         lat: loc.lat,
                         lng: loc.lng,
-                        img: loc.img || img,
+                        img: loc.img ? getMediaPath(loc.img) : img,
                         label: locs.length > 1 ? `${post.id}${letters[locIndex]}` : `${post.id}`,
                         title: post.title,
                         date: post.date,
